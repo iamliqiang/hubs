c0d1bd4dbfb0d2f59c2c362c255bea8bafd5b4ef
maxatbj <iamliqiang@users.noreply.github.com>
2022-06-07 13:17:34 +0800

    Update webpack.config.js

--- admin/webpack.config.js
+++ admin/webpack.config.js
@@ -117,7 +117,7 @@ module.exports = (env, argv) => {
     devtool: argv.mode === "production" ? "source-map" : "inline-source-map",
     devServer: {
       https: createHTTPSConfig(),
-      host: process.env.HOST_IP || "0.0.0.0",
+      host: "0.0.0.0",
       port: process.env.PORT || "8989",
       public: `${host}:${process.env.PORT || "8989"}`,
       useLocalIp: true,
